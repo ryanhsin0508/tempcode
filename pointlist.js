f9633d51b99016e138b8460cd4e970c7cfbf34ab
sean.hsin <sean.hsin@presslogic.com>
2020-08-12 12:14:42 +0800

    crm point list

---
+++ pointlist.js
@@ -0,0 +1,74 @@
+[
+    {
+        group: "特別任務",
+        list: [
+            {
+                icon: "7-days.png",
+                title: "連續7日簽到",
+                point: [2, 2, 2, 3, 3, 4, 4],
+                totalPoint: 20,
+                desc: "",
+                currentDay: 1,
+                hasRedeem: false,
+                btn: "簽到",
+                btnDone: "已簽到"
+            },
+            {
+                icon: "member.png",
+                title: "迎新註冊優惠",
+                point: [200],
+                totalPoint: 200,
+                desc: "",
+                hasRedeem: false,
+                btn: "領取",
+                btnDone: "已領取"
+            },
+            {
+                icon: "bookmark.png",
+                title: "文章收藏",
+                point: [5],
+                totalPoint: 5,
+                desc: "成功收藏5篇文章，即可獲取積分！",
+                hasRedeem: false,
+                btn: "去完成 >",
+                btnDone: "已完成",
+                action: "goBookmark"
+            }
+        ]
+    },
+    {
+        group: "一般任務",
+        list: [
+            {
+                icon: "punch-time.png",
+                title: "每日打卡",
+                point: [2],
+                totalPoint: 2,
+                desc: "",
+                hasRedeem: false,
+                btn: "領取",
+                btnDone: "已領取"
+            },
+            {
+                icon: "member.png",
+                title: "迎新註冊優惠",
+                point: [100],
+                totalPoint: 100,
+                desc: "",
+                hasRedeem: false,
+                btn: "領取",
+                btnDone: "已領取"
+            },
+            {
+                icon: "b-dgift.png",
+                title: "生日獎賞",
+                point: [50],
+                totalPoint: 50,
+                desc: "",
+                hasRedeem: false,
+                btn: "領取",
+                btnDone: "已領取"
+            }
+        ]
+    }
+]
